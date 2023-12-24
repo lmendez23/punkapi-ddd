2a87c70e47541a27bfeed0f9a3d09ed636ab07b7
Luis Méndez <lmendez@quadram.mobi>
2023-12-24 17:21:40 +0100

    Add doc to parameters and responses

--- src/Beer/Infrastructure/Symfony/Controller/BeerByFoodController.php
+++ src/Beer/Infrastructure/Symfony/Controller/BeerByFoodController.php
@@ -8,12 +8,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 class BeerByFoodController extends AbstractController
 {
     /**
      * @throws Exception
      */
+    #[OA\Get(
+        path: '/api/beers',
+        summary: 'Get a paginated list of beers and/or filtered by food string',
+        tags: ['Beers']
+    )]
+    #[OA\Parameter(
+        name: 'food',
+        description: 'food filter (3 characters min)',
+        in: 'query',
+        schema: new OA\Schema(type: 'string')
+    )]
+    #[OA\Parameter(
+        name: 'page',
+        description: 'page number',
+        in: 'query',
+        schema: new OA\Schema(type: 'integer')
+    )]
+    #[OA\Parameter(
+        name: 'per_page',
+        description: 'number of elements for each page',
+        in: 'query',
+        schema: new OA\Schema(type: 'integer')
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the beer detail',
+    )]
     #[Route('/api/beers', name: 'beers_by_food', methods: ['GET'])]
     public function __invoke(Request $request, BeersByFood $beersByFood): JsonResponse
     {
--- src/Beer/Infrastructure/Symfony/Controller/BeerByIdController.php
+++ src/Beer/Infrastructure/Symfony/Controller/BeerByIdController.php
@@ -9,12 +9,26 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 class BeerByIdController extends AbstractController
 {
     /**
      * @throws Exception
      */
+    #[OA\Get(
+        path: '/api/beers/{id}',
+        summary: 'Get the detail of a beer using the beer ID',
+        tags: ['Beers'],
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the beer detail',
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Beer not found'
+    )]
     #[Route('/api/beers/{id}', name: 'beers_by_id', methods: ['GET'])]
     public function __invoke(Request $request, BeersById $beersById, $id): JsonResponse
     {
